9a579e31dc56e80934e40664b4b1a5b70bd3d91e
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2021-12-31 11:34:03 -0300

    Adicionar arquivos de projeto.

---
+++ Multiplos de 3 ou 5/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000");
+
+double soma = 0;
+double resultado;
+
+for (double i = 0; i < 1000; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        soma = soma + i;
+        Console.WriteLine();
+    }
+}
+resultado = soma;
+Console.WriteLine(resultado);
